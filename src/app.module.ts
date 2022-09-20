80d44c3301223fe0b86c50621bf7fd30822ff0d5
Jerbeson Vinycius <jerbesonvinycius@gmail.com>
2022-09-19 21:25:21 -0300

    :wrench: Added TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { join } from 'path';
+import * as dotenv from 'dotenv';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UsersModule } from './modules/users/users.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
+dotenv.config();
+console.log(+process.env.TYPEORM_PORT);
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      synchronize: true,
+      logging: true,
+      entities: [],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       sortSchema: true,
